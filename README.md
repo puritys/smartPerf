36e4dbc5ebea2233c51b6872669d19cd5e4d178b
puritys <puritys.chen@outlook.com>
2015-07-23 13:11:28 +0800

    add node source code

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Smart Performance library
 #include "smartPerf.h"
 int main () {
     smartPerf::start("Complicated Process");
-    run_function1();
+    run_test_function1();
     clock_t timeDiff = smartPerf::end(false);
 
     smartPerf::start("Simple Process");
@@ -26,4 +26,30 @@ The sample result:
 |  Complicated Process |         20 ms        |
 |    Simple Process    |        200 ms        |
 +---------------------------------------------+
+</pre>
+
+* Node Example
+<pre>
+var smartPerf = require("smart-perf");
+
+smartPerf.start("Linear search");
+run_linear_search();
+smartPerf.end(false);
+
+smartPerf.start("Binary search");
+run_binary_search();
+smartPerf.end(false);
+
+
+smartPerf.print();
+
+</pre>
+
+The sample result:
+
+<pre>
+Key            CPU(ms)
+-------------  -------
+Linear search  207 ms
+Binary search  129 ms
 </pre>
---
+++ src/node/smartPerf.js
@@ -0,0 +1,44 @@
+var Table = require('easy-table');
+var timeData = {};
+var recordData = [];
+var lastItemName = "";
+
+
+exports.start = function (key) {
+    var startTime;
+    if (key == undefined) key = "default";
+    startTime = (new Date()).getTime();
+    timeData[key] = startTime;
+    lastItemName = key;
+}
+
+exports.end = function (display) {
+    var startTime, endTime, diffMs, key;
+    display = (display == undefined) ? true: display ;
+    key = (timeData[lastItemName]) ? lastItemName : 'default' ;
+    startTime = timeData[key];
+    endTime = (new Date()).getTime();
+    diffMs = parseInt(endTime, 10) - parseInt(startTime, 10);
+    recordData.push({key: key, time: diffMs});
+    if (true === display) {
+        console.log(lastItemName + ": time diff = " + diffMs +" ms");
+    }
+}
+
+exports.print = function () {
+    var t = new Table();
+
+    for (var i in recordData) {
+        rec = recordData[i];
+        t.cell('Key', rec.key);
+        t.cell('CPU(ms)', rec.time  + " ms");
+         t.newRow()
+    }
+    console.log(t.toString());
+}
+
+exports.clear = function () {
+    timeData = {};
+    recodrData = [];
+}
+
